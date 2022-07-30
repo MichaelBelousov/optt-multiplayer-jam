755f673d1e9aa03d53c74407409f812b26aa7cda
Emily <tweetychrip@gmail.com>
2022-07-29 18:43:43 -0700

    whee hamster speen

---
+++ src/hamster_wheel.gd
@@ -0,0 +1,33 @@
+extends RigidBody2D
+
+
+const BASE_RADIUS := 10
+const BASE_IMPULSE := 200
+const JUMP_FORCE := 25
+
+export var Hamster: PackedScene
+
+
+func _ready() -> void:
+	$RayCast2D.set_as_toplevel(true)
+	for i in range(10):
+		add_hamster()
+
+
+func add_hamster() -> void:
+	var h := Hamster.instance()
+	add_child(h)
+	for child in get_children():
+		if child is Camera2D:
+			continue
+		child.position = Vector2(BASE_RADIUS * (get_child_count() - 3), 0).rotated((child.get_index() - 1.0) / (get_child_count() - 2) * (2 * PI))
+		child.rotation = (child.get_index() - 1.0) / (get_child_count() - 2) * (2 * PI)
+
+
+func _physics_process(delta: float) -> void:
+	var input := Vector2(Input.get_axis("move_left", "move_right"), 0)
+	apply_impulse(-input * BASE_RADIUS * (get_child_count() - 3), input * BASE_IMPULSE * (get_child_count() - 2) * delta)
+	if Input.is_action_pressed("move_up") and $RayCast2D.is_colliding():
+		apply_impulse(-Vector2.UP * BASE_RADIUS * (get_child_count() - 3), Vector2.UP * JUMP_FORCE * (get_child_count() - 2))
+	$RayCast2D.position = position
+	$RayCast2D.cast_to = Vector2(0, BASE_RADIUS * (get_child_count() - 3) + 64)
