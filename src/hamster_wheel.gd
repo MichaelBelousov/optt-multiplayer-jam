e33f243ca4c3b42f30073a97ba6b8134a3f1e150
Michael Belousov <mike.belousov@bentley.com>
2022-07-31 16:44:17 -0400

    synchronize input state to master

--- src/hamster_wheel.gd
+++ src/hamster_wheel.gd
@@ -7,9 +7,12 @@ export var JUMP_FORCE := 1.0
 
 export var Hamster: PackedScene
 
+remote var p2_x_input := 0.0
+remote var p2_space_state := false
 
 func _ready() -> void:
-	rpc_config("global_position", MultiplayerAPI.RPC_MODE_REMOTE)
+	rset_config("global_position", MultiplayerAPI.RPC_MODE_MASTERSYNC)
+
 	$RayCast2D.set_as_toplevel(true)
 	for _i in 10:
 		add_hamster()
@@ -35,19 +38,22 @@ func set_hamster_position() -> void:
 		child.position = Vector2(BASE_RADIUS * (get_child_count() - 3), 0).rotated((child.get_index() - 1.0) / (get_child_count() - 2) * (2 * PI))
 		child.rotation = (child.get_index() - 1.0) / (get_child_count() - 2) * (2 * PI)
 
-
 func _physics_process(delta: float) -> void:
 	# TODO: duplicate input values by synchronized input of other player(s)
-	var input := Vector2(Input.get_axis("move_left", "move_right"), 0)
+	var p1_input = Input.get_axis("move_left", "move_right")
+	var p2_input = Input.get_axis("p2_move_left", "p2_move_right")
+	var input := Vector2(p1_input + p2_input, 0)
 	apply_impulse(-input * BASE_RADIUS * (get_child_count() - 3), input * BASE_IMPULSE * (get_child_count() - 2) * delta)
 	if Input.is_action_pressed("move_up") and $RayCast2D.is_colliding():
 		apply_impulse(-Vector2.UP * BASE_RADIUS * (get_child_count() - 3), Vector2.UP * JUMP_FORCE * (get_child_count() - 2))
 	$RayCast2D.position = position
 	$RayCast2D.cast_to = Vector2(0, BASE_RADIUS * (get_child_count() - 3) + 64)
 
+
 func _process(_delta: float) -> void:
 	if is_network_master():
 		rset_unreliable("global_position", global_position)
 	else:
-		# TODO: synchronize input state to master (per connection?)
-		pass
+		# NOTE: may want to do this on _input for less lag?
+	  rset_unreliable("p2_x_input", Input.get_axis("p2_move_left", "p2_move_right"))
+	  rset_unreliable("p2_space_state", Input.is_action_pressed("p2_move_up"))
