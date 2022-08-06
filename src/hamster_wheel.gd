8f0a56076c2dfd78433edc75f40a649d212b246f
Michael Belousov <mike.belousov@bentley.com>
2022-08-05 21:54:47 -0400

    tweak physics and make both players jumping increase jump force

--- src/hamster_wheel.gd
+++ src/hamster_wheel.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
 
+export var RAY_BASE_SIZE := 2.0
 export var BASE_RADIUS := 3.0
 export var BASE_IMPULSE := 50.0
-export var JUMP_FORCE := 1.0
+export var BASE_JUMP_FORCE := 8.0
+## The multiplier of motion force when not touching the ground
+export var AIR_MOTION_FACTOR := 0.4
 
 export var Hamster: PackedScene
 
@@ -58,21 +61,36 @@ func set_hamster_position() -> void:
 		child.rotation = (child.get_index() - 1.0) / (get_child_count() - 2) * (2 * PI)
 
 
+## convert a boolean to an int that can be added
+static func boolscalar(b: bool) -> int:
+	return 1 if b else 0
+
+
 func _physics_process(delta: float) -> void:
-	# TODO: duplicate input values by synchronized input of other player(s)
+	var touching_ground = $RayCast2D.is_colliding()
+
+	# handle left-right movement
 	var p1_input = Input.get_axis("move_left", "move_right")
 	var p2_input = Input.get_axis("p2_move_left", "p2_move_right") if is_p2_local else p2_remote_x_input
 	var input := Vector2(p1_input + p2_input, 0)
-	apply_impulse(-input * BASE_RADIUS * (get_child_count() - 3), input * BASE_IMPULSE * (get_child_count() - 2) * delta)
-	var is_jump_pressed = (
-		Input.is_action_pressed("move_up")
-		or (is_p2_local and Input.is_action_pressed("p2_move_up"))
-		or p2_remote_jump_input
+	var air_touch_factor = 1.0 if touching_ground else AIR_MOTION_FACTOR
+	apply_impulse(
+		-input * BASE_RADIUS * (get_child_count() - 3),
+		input * air_touch_factor * BASE_IMPULSE * (get_child_count() - 2) * delta
 	)
-	if is_jump_pressed and $RayCast2D.is_colliding():
-		apply_impulse(-Vector2.UP * BASE_RADIUS * (get_child_count() - 3), Vector2.UP * JUMP_FORCE * (get_child_count() - 2))
+
+  # handle jump
+	var jump_strength = (
+		boolscalar(Input.is_action_pressed("move_up"))
+		+ boolscalar((is_p2_local and Input.is_action_pressed("p2_move_up")) or p2_remote_jump_input)
+	)
+	if jump_strength > 0 and touching_ground:
+		apply_impulse(
+			-Vector2.UP * BASE_RADIUS * (get_child_count() - 3),
+			Vector2.UP * jump_strength * BASE_JUMP_FORCE * (get_child_count() - 2)
+		)
 	$RayCast2D.position = position
-	$RayCast2D.cast_to = Vector2(0, BASE_RADIUS * (get_child_count() - 3) + 64)
+	$RayCast2D.cast_to = Vector2(0, BASE_RADIUS * (get_child_count() - 3) + RAY_BASE_SIZE)
 
 
 func _process(_delta: float) -> void:
