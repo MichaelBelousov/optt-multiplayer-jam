325e7456d02e85db4c5b18417422f26236f0b08a
CollinxAnderson <canderson819@gmail.com>
2022-08-06 16:04:30 -0500

    Update hamster_wheel.gd

--- src/hamster_wheel.gd
+++ src/hamster_wheel.gd
@@ -90,9 +90,9 @@ func _physics_process(delta: float) -> void:
 
   if touching_ground and jump_strength > 0 and jump_start_time == null:
     jump_start_time = OS.get_ticks_msec()
-    if jump_strength <= 1:
+    if jump_strength == 1 and !$sfx_Jump_Single.is_playing():
       $sfx_Jump_Single.play()
-    else:
+    if jump_strength == 2 and !$sfx_Jump_Single.is_playing():
       $sfx_Jump_Double.play()
   if jump_start_time != null and OS.get_ticks_msec() - jump_start_time > JUMP_IMPULSE_DURATION_MS:
     jump_start_time = null
